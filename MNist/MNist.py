3827903de73216eaefc3f9deafc4d31a209445ee
iori <iorikyo79@gmail.com>
2017-02-16 14:40:51 +0900

    돌아가는 소스로 변경

--- MNist/MNist.py
+++ MNist/MNist.py
@@ -1,76 +1,43 @@
+#from __future__ import print_function
+
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
 
-n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
+# Parameter
+logs_path = 'c:/board/example_mnist'
+x = tf.placeholder(tf.float32, [None, 784], name='Input')
+y_ = tf.placeholder(tf.float32, [None, 10], name='Label')
 
-n_classes = 10
-batch_size = 100
+W = tf.Variable(tf.zeros([784, 10]), name='Weight')
+b = tf.Variable(tf.zeros([10]), name='Bias')
 
-x = tf.placeholder('float', [None, 784])
-y = tf.placeholder('float')
+# Model
+with tf.name_scope('Model'):
+    y = tf.nn.softmax(tf.matmul(x, W) + b)
+with tf.name_scope('Loss'):
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+with tf.name_scope('SGD'):
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+with tf.name_scope('Accuracy'):
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-def neural_network_model(data):
-    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+#sess = tf.InteractiveSession()
+init = tf.global_variables_initializer()
 
-    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+tf.summary.scalar('loss', cross_entropy)
+tf.summary.scalar('accuracy', accuracy)
+merged_summary_op = tf.summary.merge_all()
 
-    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
+with tf.Session() as sess:
+    sess.run(init)
 
-    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                    'biases':tf.Variable(tf.random_normal([n_classes])),}
+    summary_writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
 
+    for i in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        _, c, a, summary = sess.run([train_step, cross_entropy, correct_prediction, merged_summary_op], feed_dict={x:batch_xs, y_:batch_ys})
+        summary_writer.add_summary(summary, i)
 
-    l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']), hidden_1_layer['biases'])
-    l1 = tf.nn.relu(l1)
-
-    l2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']), hidden_2_layer['biases'])
-    l2 = tf.nn.relu(l2)
-
-    l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']), hidden_3_layer['biases'])
-    l3 = tf.nn.relu(l3)
-
-    output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
-
-    return output
-
-def train_neural_network(x):
-    prediction = neural_network_model(x)
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
-    
-    accuracy = 0
-    tf.summary.scalar('accuracy', accuracy) ###
-    tf.summary.merge_all()  ###
-
-    hm_epochs = 10
-    with tf.Session() as sess:
-        summary_writer = tf.summary.FileWriter('/path/to/logs', sess.graph)        
-        
-        sess.run(tf.global_variables_initializer())
-
-        for epoch in range(hm_epochs):
-            epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples/batch_size)):
-                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
-                epoch_loss += c
-
-            print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
-
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        summary_writer.add_summary(accuracy, epoch)
-        summary_writer.flush()
-        print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
-            
-
-train_neural_network(x)
-
-#sub1
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
